de0e1dc28b1e3487ce8f25e56708f0cda34b00dd
Angelo Moroni <mor.angelo.mor@gmail.com>
2016-06-07 11:55:25 +0200

    alias Adapter

---
+++ app/src/main/java/com/angelomoroni/coordinatorlayoutexample/AliasAdapter.java
@@ -0,0 +1,25 @@
+package com.angelomoroni.coordinatorlayoutexample;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+/**
+ * Created by debug on 07/06/16.
+ */
+public class AliasAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
