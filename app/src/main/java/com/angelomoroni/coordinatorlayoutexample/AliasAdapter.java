176bda4899e36544e4d051aae40dcae6ec1c4394
Angelo Moroni <mor.angelo.mor@gmail.com>
2016-06-07 12:04:57 +0200

    constructoralias adapter

--- app/src/main/java/com/angelomoroni/coordinatorlayoutexample/AliasAdapter.java
+++ app/src/main/java/com/angelomoroni/coordinatorlayoutexample/AliasAdapter.java
@@ -1,12 +1,33 @@
 package com.angelomoroni.coordinatorlayoutexample;
 
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 import android.view.ViewGroup;
+
+import java.util.ArrayList;
 
 /**
  * Created by debug on 07/06/16.
  */
 public class AliasAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
+    public View header;
+    public View footer;
+    public String mainEmail,mainPhoneNumber;
+    public ArrayList<String> emails,phones;
+    public int sections[] = new int[2];
+
+    public AliasAdapter(View header, View footer, String mainEmail, String mainPhoneNumber) {
+        this.header = header;
+        this.footer = footer;
+        this.mainEmail = mainEmail;
+        this.mainPhoneNumber = mainPhoneNumber;
+        emails = new ArrayList<>();
+        phones = new ArrayList<>();
+        sections[0] = 1;
+        sections[1] = 2+emails.size();
+    }
+
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
 
@@ -20,6 +41,6 @@ public class AliasAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> 
 
     @Override
     public int getItemCount() {
-        return 0;
+        return 6+emails.size()+phones.size();
     }
 }
